c85e6c9b4071777f389ea4f272783ba68b841314
Samuel Keno <sgere195@gmail.com>
2023-09-02 17:10:18 +0300

    handeling docx files

--- quiz_app/file_processor/file_handler.py
+++ quiz_app/file_processor/file_handler.py
@@ -2,6 +2,7 @@ import PyPDF2
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
+from docx import Document
 
 class FileHandler:
     def __init__(self, file) -> None:
@@ -24,17 +25,31 @@ class FileHandler:
             self.text += pdf_reader.pages[page].extract_text().strip()
         return self.text
     
-
-    def summerized(self, num_tokens):
+    def read_docx(self, start_page = 0, end_page = 0 ):
+        document = Document(self.file)
+        paragraphs = document.paragraphs
+        num_pages = len(paragraphs)
+        if end_page == 0:
+            end_page = num_pages - 1
+        if end_page >= num_pages:
+            end_page = num_pages - 1
+        extracted_paragraphs = paragraphs[start_page:end_page+1]
+        extracted_text = [paragraph.text for paragraph in extracted_paragraphs]
+        final = " ".join(extracted_text)
+        self.text = final.strip()
+        return final.strip()
+    
+    def summerized(self):
         parser = PlaintextParser.from_string(self.text, Tokenizer("english"))
         summary = self.summerizer(parser.document, len(self.text)/10)
         summ = ""
-        token_count = 0
-        flag = False
         for word in summary:
                 summ += str(word)
         return summ
 
 
 
-
+file = FileHandler("/home/samuel/Desktop/SamuelLegesseLAB3.docx")
+print(file.read_docx(0, 5))
+print("\n\nsummerized\n")
+print(file.summerized())
