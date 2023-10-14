1a812ec2b3514437a7a90a90655fa378529409ae
Abdi <inventorabdi@gmail.com>
2023-10-14 01:41:28 -0700

    views.py code cleanedup

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
    # path('quiz', views.home, name="quiz"),
     path('register', views.user_register, name="register"),
     path('login', views.user_login, name='login'),
-    path('test', views.test, name="test"),
     path('chat/<str:query>', views.chat, name='char'),
     path('get_chat/', views.get_chat, name="get_chat"),
     path('logout/', views.user_logout, name="logout"),
@@ -16,7 +15,7 @@ urlpatterns = [
     
     path('home', views.home, name="home"),
     path('upload', views.upload, name="upload"),
-    path('quiz/', views.quiz, name="quiz"),
     path('chat', views.chat, name="chat"),
     path('my_quizes', views.myquizes, name='my_quizes'),
+    path("handle_quiz_submit", views.handle_quiz_submit, name="handle_quiz_submit"),
 ]
