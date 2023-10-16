0c3e4350efac8d58f69599e97da7440d3531949d
Abdi <inventorabdi@gmail.com>
2023-10-16 10:42:24 -0700

    just to cleanup

---
+++ quiz_app/api/api_setup_v2.py
@@ -0,0 +1,17 @@
+multiple_Choice_question_format = '''
+1.What is Django?
+A. A programming language
+B. A web framework
+C. A database management system
+D. A version control system
+CorrectOption:OptionA
+Explanation: Django is a web framework written in Python that simplifies the process of building web applications.
+
+$$$$$$$$$$
+2.What is the purpose of Django's ORM (Object-Relational Mapping)?
+A. To provide a graphical user interface for Django projects
+B. To handle URL routing in Django applications
+C. To interact with the database using Python objects
+D. To manage the authentication and authorization in Django
+Explanation: Django's ORM allows developers to interact with the database using Python code instead of writing raw SQL queries.
+'''
