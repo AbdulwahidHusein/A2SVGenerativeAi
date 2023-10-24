55d10008c7035a682e44ac476955700c8d614c15
Samuel Keno <sgere195@gmail.com>
2023-10-24 14:46:44 +0300

    cleaning the bot" "

--- telebot/bot.py
+++ telebot/bot.py
@@ -1,12 +1,19 @@
 import logging
 import os
 import openai
+import sys
+
+from quiz_app import generator
+
+sys.path.append("..")
 from telegram import (
     Message,
     ChatAction,
+    Poll,
     Update,
     InlineKeyboardButton,
     InlineKeyboardMarkup,
+    File,
 )
 from telegram.ext import (
     ConversationHandler,
@@ -17,105 +24,36 @@ from telegram.ext import (
     Updater,
     CallbackQueryHandler,
 )
-from telegram import Bot
-import file_handler
 from dotenv import load_dotenv
-import json
+
 
 load_dotenv()
-question_format = {
-    "JS": [
-        {
-            "id": 1,
-            "question": "Question number 1",
-            "options": ["option a", "option b", "option c", "option d"],
-            "answer": "option c",
-            "score": 0,
-            "status": "",
-            "user_answer": "",
-            "explanation": "brief explanation",
-        },
-        {
-            "id": 2,
-            "question": "Question number 2",
-            "options": ["option a", "option b", "option c", "option d"],
-            "answer": "option c",
-            "score": 0,
-            "status": "",
-            "user_answer": "",
-            "explanation": "brief explanation",
-        },
-    ]
-}
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
-
-
 TOKEN = os.environ.get("BOT_TOKEN")
 
 logging.basicConfig(level=logging.INFO)
 
+# some global variables for storing data.
 user_data = {}
-start_flag = 0
-difficulty = "not difficult"
+question_format = {}
+user_answers = []
+START_PAGE = 0
+END_PAGE = 1
 
 
 def help(update: Update, context: CallbackContext):
     update.message.reply_text("Please enter file")
 
 
+# The start function
 def start(update: Update, context: CallbackContext):
     update.message.reply_text(
         "Hello There, please enter file so we can change it to qize for you!"
     )
 
 
-def create_buttons(num_pages):
-    buttons = []
-
-    for page_num in range(1, num_pages + 1):
-        button = InlineKeyboardButton(str(page_num), callback_data=str(page_num))
-
-        buttons.append(button)
-
-    return buttons
-
-
-def create_keyboard_markup(buttons, buttons_per_row=2):
-    keyboard = []
-    row = []
-    for i, button in enumerate(buttons, start=1):
-        row.append(button)
-        if i % buttons_per_row == 0 or i == len(buttons):
-            keyboard.append(row)
-            row = []
-    return InlineKeyboardMarkup(keyboard)
-
-
-def Enterfile(update: Update, context: CallbackContext):
-    file = update.message.document
-    file_recieved = context.bot.get_file(file_id=file.file_id)
-    file_name = file.file_name
-    file_size = file.file_size
-
-    file_recieved.download(file_name)
-    update.message.reply_text("Successs")
-
-    reply_text = f"Recieved file: {file_name}\nFile size : {file_size} \n\nPlease choose the start page:"
-
-    global user_data
-    try:
-        user_data["file_name"] = file_name
-    except:
-        user_data.setdefault("file_name", file_name)
-
-    text = file_handler.FileHandler(file_name)
-    number_pages = text.page_num(file_name)
-    buttons = create_buttons(num_pages=number_pages)
-    keyboard_markup = create_keyboard_markup(buttons=buttons)
-    update.message.reply_text(reply_text, reply_markup=keyboard_markup)
-
-
+# displays the difficulty buttons
 def set_difficulty(update: Update, context: CallbackContext):
     keyboard = [
         [InlineKeyboardButton("Easy", callback_data="easy")],
@@ -123,91 +61,131 @@ def set_difficulty(update: Update, context: CallbackContext):
         [InlineKeyboardButton("Difficult", callback_data="difficult")],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    return reply_markup
+    update.message.reply_text("Choose Difficulty:", reply_markup=reply_markup)
 
 
-def show_difficulty(update: Update, context: CallbackContext):
-    keyboard_markup = set_difficulty(update=update, context=context)
-    reply_text = "choose difficulty:"
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text=reply_text, reply_markup=keyboard_markup
+# function for setting the end page of the recieved file
+def set_end(update: Update, context: CallbackContext):
+    end = update.message.text
+    user_data["end_page"] = int(end)
+    update.message.reply_text(
+        f"Start page: {user_data['start_page']}\nEnd page: {user_data['end_page']}"
     )
+    set_difficulty(update=update, context=context)
 
 
+# function for setting the start page of the received file
+def set_start(update: Update, context: CommandHandler):
+    start = update.message.text
+    user_data["start_page"] = int(start)
+    update.message.reply_text("Enter end page")
+    return END_PAGE
+
+
+def cancel(update: Update, context: CallbackContext):
+    update.message.reply_text("Conversation canceled.")
+    return ConversationHandler.END
+
+
+# Function for processing the file. It currently works by downloading the file first and then moves on to processing it.
+# working on ways to process the file without downloading it
+def Enterfile(update: Update, context: CallbackContext):
+    global user_data
+    file = update.message.document
+    file_recieved = context.bot.get_file(file_id=file.file_id)
+    file_name = file.file_name
+    user_data["file_name"] = file_name
+    file_downloaded = file_recieved.download()
+    user_data["file"] = file_downloaded
+    with open(file_downloaded, "rb") as f:
+        user_data["file"] = file_downloaded
+
+    reply_text = f"Recieved file: {user_data['file_name']}"
+    user_data["file_name"] = file_name
+    context.bot.send_chat_action(
+        chat_id=update.effective_chat.id, action=ChatAction.TYPING
+    )
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=reply_text + "\nEnter start page:",
+    )
+    return START_PAGE
+
+
+# this function handles the events that happen after a button is pressed
 def button_callback(update: Update, context: CallbackContext):
     global user_data
-    global start_flag
     global difficulty
     query = update.callback_query
-    try:
-        page_number = int(query.data)
-    except:
-        command = query.data
-    if query.data.isdigit() and start_flag == 0:
-        try:
-            user_data["start_page_text"] = page_number
-        except:
-            user_data.setdefault("start_page_text", page_number)
-        start_flag = 1
+    if query.data.isalnum():
+        user_data["difficulty"] = query.data
         query.message.reply_text(
-            text=f"start page set to {user_data['start_page_text']}",
+            f"diffiiculty set to {user_data['difficulty']}",
         )
-        logging.info("start page set")
-    elif query.data.isdigit() and start_flag == 1:
-        try:
-            user_data["end_page_text"] = page_number
-        except:
-            user_data.setdefault("end_page_text", page_number)
-        query.message.reply_text(
-            f"End page set to {user_data['end_page_text']}",
-        )
-        logging.info("end page set")
-        start_flag = 0
-        show_difficulty(update=update, context=context)
-    else:
-        difficulty = query.data
-        user_data["difficulty"] = difficulty
         send_request(update=update, context=context)
 
 
 def send_request(update: Update, context: CallbackContext):
-    text = file_handler.FileHandler(user_data["file_name"])
-    start_page = int(user_data["start_page_text"])
-    end_page = int(user_data["end_page_text"])
-    text.read_file(spage=start_page, epage=end_page)
-    processed_string = text.summerized()
-    number_of_questions = 5
-    user_message = f"""generate a quiz contining {number_of_questions} different multiple choice questions with 
-    different context containing four choices with {user_data['difficulty']} difficulty the questions must be returned in 
-    the following format {question_format} note the 
-    question must be in json format!!! also make sure the explanations must be less than 2 lines important!.
-    NOTE only use the following text for the generation of quiz {processed_string}"""
-    print(user_data["difficulty"])
     context.bot.send_chat_action(
         chat_id=update.effective_chat.id, action=ChatAction.TYPING
     )
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=user_message,
-        max_tokens=2000,
-        temperature=0.1,
-        n=1,
-        stop=None,
-    )
-    bot_response = response.choices[0].text.strip()
-    json_start_index = bot_response.find("{'JS':")
-    json_text = bot_response[json_start_index:]
+    global user_data
+    global question_format
+    with open(user_data["file"], "rb") as f:
+        question_format = generator.get_question(
+            f,
+            5,
+            user_data["difficulty"],
+            int(user_data["start_page"]),
+            int(user_data["end_page"]),
+            mode="multiple_choice",
+            model="chatgpt",
+        )
+    logging.info("request sent")
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text=json_text)
+    # context.bot.send_message(chat_id=update.effective_chat.id, text=question_format)
+    for question_data in question_format["questions"]:
+        question_text = question_data["question"]
+        options = [
+            question_data["optionA"],
+            question_data["optionB"],
+            question_data["optionC"],
+            question_data["optionD"],
+        ]
+        correct_option = question_data["correctOption"]
+
+        try:
+            context.bot.send_poll(
+                chat_id=update.effective_chat.id,
+                question=question_text,
+                options=options,
+                type=Poll.QUIZ,
+                correct_option_id=ord(correct_option[-1]) - ord("A"),
+                is_anonymous=False,
+                explanation=f'Correct Answer: {question_data["explanation"]}',
+                open_period=60,
+            )
+        except:
+            pass
 
 
 def register(dispatcher):
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.document & ~Filters.text, Enterfile))
+    conv_handler = ConversationHandler(
+        entry_points=[MessageHandler(Filters.document, Enterfile)],
+        states={
+            START_PAGE: [MessageHandler(Filters.text & ~Filters.command, set_start)],
+            END_PAGE: [MessageHandler(Filters.text & ~Filters.command, set_end)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+    dispatcher.add_handler(conv_handler)
     dispatcher.add_handler(CallbackQueryHandler(button_callback))
+
     dispatcher.add_handler(CommandHandler("help", help))
 
 
+# driver function
 def main():
     updater = Updater(token=TOKEN)
     register(updater.dispatcher)
