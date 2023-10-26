86aac771994ff2462f36f29742dc7babf2541d9b
Abdi <inventorabdi@gmail.com>
2023-10-26 00:10:44 -0700

    fixxed some git ignore issues

--- testsummerize.py
+++
@@ -1,8 +0,0 @@
-from yake import KeywordExtractor
-
-ext = KeywordExtractor(top=10, stopwords=None)
-keywords = ext.extract_keywords("""/ /  model, and Niels Bohr's planetary model. These models have contributed to our understanding of the structure and behavior of atoms [2].
-
-isciplines, including chemistry, physics, materials science, and biochemistry.""")
-
-print(keywords)
