6d56b50b762c65c47fa4f9433537c6f0be81f62f
Abdi <inventorabdi@gmail.com>
2023-10-26 22:44:02 -0700

    ui modifications

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -2,18 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   # path('quiz', views.home, name="quiz"),
     path('register', views.user_register, name="register"),
     path('login', views.user_login, name='login'),
-    
     path('get_chat/', views.get_chat, name="get_chat"),
     path('logout/', views.user_logout, name="logout"),
-    
-    
-    
-    #new paths
     path('quiz/<int:id>', views.get_quiz, name="quiz"),
-    path('home', views.home, name="home"),
+    path('', views.home, name="home"),
     path('upload', views.handle_upload, name="upload"),
     path('chat/', views.chat, name="chat"),  
     path('my_quizes', views.myquizes, name='my_quizes'),
